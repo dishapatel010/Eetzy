78aafbc6442e77d38e6f686dfb1c6eb6171e20f0
amankumarm <amankumarm441@gmail.com>
2021-02-23 00:04:54 +0530

    cleaning

--- app.js
+++ app.js
@@ -26,70 +26,7 @@ app.use('/cli/',cli)
 app.use('/web/',web)
 app.get('/',(req,res)=>{
 
-    res.sendFile(__dirname+'/templates/index.html')
+    res.redirect('/web/')
 })
-
-app.post('/addfile',async (req,res)=>{
-
-    
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.');
-  }
-  let file = req.files.file
-  let fid=await getseq()
-  new File({id:await getseq(),name:file.name,path:`/Data/${file.name}`}).save()
-  .then(resp=>fid=resp.id)
-  .catch(err=>res.status(500).send(err))
-  file.mv(`./Data/${file.name}`,function(err){
-      if (err) {
-          return res.status(500).send(err)
-        }
-        res.send('Uploaded file id - '+fid)
-  })
-})
-
-app.get('/viewbyid/:id',(req,res)=>{
-
-    File.find({id:req.params.id})
-    .then(resp=>{
-    res.sendFile(__dirname+resp[0].path)
-    })
-    .catch(err=>console.log(err))
-})
-
-app.get('/viewbyname/:name',async (req,res)=>{
-    var allfiles;
-    var nameparam=req.params.name
-    console.log(nameparam)
-    await File.find()
-    .then(result=>allfiles = result)
-    .catch(err=>res.status(500).send(err))
-    var resp=[]
-    const l=allfiles.length
-    for(let i=0;i<l;i++){
-        if (allfiles[i].name.includes(nameparam) && allfiles.length!=0) {
-            var x=allfiles[i]
-            x.path=__dirname+x.path
-            resp.push(x)
-        }
-    }
-    // console.log(resp)
-    res.render('sr',{resp:resp})
-})  
-
-app.get('/viewfile/:fname',(req,res)=>{
-    res.sendFile(__dirname+`/Data/${req.params.fname}`)
-})
-
-app.get('/test',async (req,res)=>{
-    res.cookie('aman','kumar',{ maxAge: 900000, httpOnly: true })
-    res.end()
-})
-app.get('/test1',async (req,res)=>{
-    console.log(req.cookies)
-    res.end()
-
-})
-
 
 app.listen(3000,()=>console.log(`port ${3000}`))
